d2c5581316cd02af0b98bf67b3a010d26a6e4721
Mikael Sloth <mikael.sloth@outlook.dk>
2026-08-06 22:20:41 +0200

    refactor: program.cs into configuration extension methods

---
+++ MyGardenPlanner2026/MyGardenPlanner2026/Configuration/Extensions/BlazorServicesExtensions.cs
@@ -0,0 +1,14 @@
+﻿namespace MyGardenPlanner2026.Configuration.Extensions;
+
+public static class BlazorServicesExtensions
+{
+    public static IServiceCollection AddBlazorServices(this IServiceCollection services)
+    {
+        services.AddRazorComponents()
+            .AddInteractiveServerComponents()
+            .AddInteractiveWebAssemblyComponents()
+            .AddAuthenticationStateSerialization();
+
+        return services;
+    }
+}
--- MyGardenPlanner2026/MyGardenPlanner2026/Program.cs
+++ MyGardenPlanner2026/MyGardenPlanner2026/Program.cs
@@ -1,71 +1,19 @@
-using Microsoft.AspNetCore.Components.Authorization;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.EntityFrameworkCore;
-using MyGardenPlanner2026.Components;
-using MyGardenPlanner2026.Components.Account;
-using MyGardenPlanner2026.Data;
+using MyGardenPlanner2026.Configuration.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-builder.Services.AddRazorComponents()
-    .AddInteractiveServerComponents()
-    .AddInteractiveWebAssemblyComponents()
-    .AddAuthenticationStateSerialization();
-
-builder.Services.AddCascadingAuthenticationState();
-builder.Services.AddScoped<IdentityRedirectManager>();
-builder.Services.AddScoped<AuthenticationStateProvider, IdentityRevalidatingAuthenticationStateProvider>();
-
-builder.Services.AddAuthentication(options =>
-    {
-        options.DefaultScheme = IdentityConstants.ApplicationScheme;
-        options.DefaultSignInScheme = IdentityConstants.ExternalScheme;
-    })
-    .AddIdentityCookies();
-
-var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
-builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseSqlServer(connectionString));
-builder.Services.AddDatabaseDeveloperPageExceptionFilter();
-
-builder.Services.AddIdentityCore<ApplicationUser>(options =>
-    {
-        options.SignIn.RequireConfirmedAccount = true;
-        options.Stores.SchemaVersion = IdentitySchemaVersions.Version3;
-    })
-    .AddEntityFrameworkStores<ApplicationDbContext>()
-    .AddSignInManager()
-    .AddDefaultTokenProviders();
-
-builder.Services.AddSingleton<IEmailSender<ApplicationUser>, IdentityNoOpEmailSender>();
+// 1. Registrer services via modulariserede extension-metoder
+builder.Services
+    .AddBlazorServices()
+    .AddDatabaseServices(builder.Configuration)
+    .AddIdentityServices();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseWebAssemblyDebugging();
-    app.UseMigrationsEndPoint();
-}
-else
-{
-    app.UseExceptionHandler("/Error", createScopeForErrors: true);
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
-}
-app.UseStatusCodePagesWithReExecute("/not-found", createScopeForStatusCodePages: true);
-app.UseHttpsRedirection();
+// 2. HTTP Request Pipeline (Middleware)
+app.UseWebPipeline();
 
-app.UseAntiforgery();
-
-app.MapStaticAssets();
-app.MapRazorComponents<App>()
-    .AddInteractiveServerRenderMode()
-    .AddInteractiveWebAssemblyRenderMode()
-    .AddAdditionalAssemblies(typeof(MyGardenPlanner2026.Client._Imports).Assembly);
-
-// Add additional endpoints required by the Identity /Account Razor components.
-app.MapAdditionalIdentityEndpoints();
+// 3. Routing & Endpoints
+app.MapRoutingEndpoints();
 
 app.Run();
